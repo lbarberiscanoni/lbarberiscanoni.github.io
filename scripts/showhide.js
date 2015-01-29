41176786090cb3bd6471840b2a288c3bff5d8265
lbarberiscanoni <hllbck7@gmail.com>
2015-01-29 00:31:43 -0800

    fixed the showhide script so that now clicking on share only fades in the specific media div

--- scripts/showhide.js
+++ scripts/showhide.js
@@ -1,3 +1,7 @@
 $("button.share").click(function(){
-	$(".media").toggle();
+	$(this).closest("div.project_card").find("div.media").fadeToggle();
+});
+
+$("button.share").click(function(){
+	$(this).closest("div.tumblr").find("div.media").fadeToggle();
 });
