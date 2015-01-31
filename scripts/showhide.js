a79f6a385daa88bdf0c453bcdb4a093525a858ed
lbarberiscanoni <hllbck7@gmail.com>
2015-01-31 15:35:39 -0800

    reworked the See Full button so that it can show either the code or the actual site

--- scripts/showhide.js
+++ scripts/showhide.js
@@ -2,6 +2,10 @@ $("button.share").click(function(){
 	$(this).closest("div.project_card").find("div.media").fadeToggle();
 });
 
+$("button.seeFull").click(function(){
+	$(this).closest("div.project_card").find("div.projectCode").fadeToggle();
+});
+
 $("button.share").click(function(){
 	$(this).closest("div.tumblr").find("div.media").fadeToggle();
 });
